9d4f6b928aece95a733b0b5270f1b0ce103432ef
chunyulin <r12942148@ntu.edu.tw>
2025-03-16 18:22:12 +0800

    Larger batch size

--- train.sh
+++ train.sh
@@ -1,4 +1,4 @@
-python python train.py --wandb_run_name dqn_fuel_1000_0.999_1024  --use_wandb --batch_size 512
+python train.py --wandb_run_name dqn_fuel_1000_0.999_1024  --use_wandb --batch_size 512
 git add .
 git commit -m "Larger batch size"
 git push
